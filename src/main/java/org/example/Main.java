b350c7ed52ce3412ad2b287e1533976b6ac36fc9
Athena K <athena3750@hotmail.com>
2023-09-11 23:50:00 -0400

    first commit of array exercices in java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,12 @@ public class Main {
         System.out.println("Hello world!");
 
         int x = 0;
-        int[] arrayNum = {1, 2, 3, 4, 5};
+        int[] arrayNum = {1, 2, 3, 4, 5, 3, 4, 7, 7};
 
         for(int i = 0; i < arrayNum.length; i++)
             System.out.println(arrayNum[i]);
 
-        int numToFind = 7status;
+        int numToFind = 7;
         int found = 0;
         int position = -1;
 
@@ -25,5 +25,31 @@ public class Main {
                 System.out.println("The number " + numToFind + " is found at the position " + position + " in the array.");
             else
                 System.out.println("The number " + numToFind + " s not found in the array.");
+
+
+    int repeatNum = 0;
+    found = 0;
+
+        for(int j = 0; j < arrayNum.length; j++) {
+            if (numToFind == arrayNum[j]) {
+                found = arrayNum[j];
+                position = j;
+                repeatNum++;
+
+            }
         }
-    }
+
+        if(found != 0 )
+            System.out.println("The number " + numToFind + " is found at the position " + position + " in the array. It was found " + repeatNum + " times in the array.");
+        else
+            System.out.println("The number " + numToFind + " s not found in the array.");
+
+        }
+
+
+
+
+
+
+}
+
